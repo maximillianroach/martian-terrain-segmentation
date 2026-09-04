aa502afebed7a06867b8aea017ed514daf9d9a54
maximillianroach <maxsquid567@gmail.com>
2026-09-04 19:38:10 -0400

    update time limit to 8 hours

--- train.sh
+++ train.sh
@@ -4,7 +4,7 @@
 #SBATCH --gpus=1
 #SBATCH --cpus-per-task=4
 #SBATCH --mem=24GB
-#SBATCH --time=02:00:00
+#SBATCH --time=08:00:00
 #SBATCH --output=logs/ai4mars_%j.out
 #SBATCH --error=logs/ai4mars_%j.err
 
