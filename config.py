e449187ada5f856ca889afc2c627f2c7812e0998
maximillianroach <maxsquid567@gmail.com>
2026-09-04 06:57:05 -0400

    update config.py

--- config.py
+++ config.py
@@ -8,6 +8,6 @@ TRAIN_LABELS = DATA_ROOT / "labels" / "train"
 TEST_LABELS = DATA_ROOT / "labels" / "test" / "masked-gold-min3-100agree"
 
 NUM_CLASSES = 4
-BATCH_SIZE = 4
+BATCH_SIZE = 8
 IMG_SIZE = 512
 NUM_EPOCHS = 50
